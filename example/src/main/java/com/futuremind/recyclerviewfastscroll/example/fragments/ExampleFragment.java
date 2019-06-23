62599fffd13d0678ed4939dc4537d5497900e439
Hossein <ho.morf@outlook.com>
2019-06-23 20:11:15 +0430

    Added support for android X

--- example/src/main/java/com/futuremind/recyclerviewfastscroll/example/fragments/ExampleFragment.java
+++ example/src/main/java/com/futuremind/recyclerviewfastscroll/example/fragments/ExampleFragment.java
@@ -1,10 +1,10 @@
 package com.futuremind.recyclerviewfastscroll.example.fragments;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,8 +23,8 @@ public abstract class ExampleFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View layout = LayoutInflater.from(getActivity()).inflate(getLayoutId(), container, false);
 
-        RecyclerView recyclerView = (RecyclerView) layout.findViewById(R.id.recyclerview);
-        fastScroller = (FastScroller) layout.findViewById(R.id.fastscroll);
+        RecyclerView recyclerView = layout.findViewById(R.id.recyclerview);
+        fastScroller = layout.findViewById(R.id.fastscroll);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         CountriesAdapter adapter = new CountriesAdapter(getActivity());
--- fastscroll/src/main/java/com/futuremind/recyclerviewfastscroll/Utils.java
+++ fastscroll/src/main/java/com/futuremind/recyclerviewfastscroll/Utils.java
@@ -11,7 +11,6 @@ public class Utils {
 
     public static float getViewRawY(View view) {
         int[] location = new int[2];
-        location[0] = 0;
         location[1] = (int) view.getY();
         ((View)view.getParent()).getLocationInWindow(location);
         return location[1];
@@ -20,7 +19,6 @@ public class Utils {
     public static float getViewRawX(View view) {
         int[] location = new int[2];
         location[0] = (int) view.getX();
-        location[1] = 0;
         ((View)view.getParent()).getLocationInWindow(location);
         return location[0];
     }
